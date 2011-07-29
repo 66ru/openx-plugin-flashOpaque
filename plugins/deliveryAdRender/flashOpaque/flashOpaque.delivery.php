6cf0ef5d3ed5a4608bcca3697117000d486279da
merge <to.merge@gmail.com>
2011-07-29 15:22:59 +0600

    initial commit 1.0

---
+++ plugins/deliveryAdRender/flashOpaque/flashOpaque.delivery.php
@@ -0,0 +1,11 @@
+<?php
+
+function Plugin_deliveryAdRender_flashOpaque_flashOpaque_delivery_postAdRender(&$aBanner)
+{
+	echo 'trololo';
+	return true;
+	// var_dump($aBanner);die;
+    // $code = !empty($aBanner['htmlcache']) ? $aBanner['htmlcache'] : '';
+	// $code.="\n   ox_swf.addParam('wmode','transparent');";
+}
+?>
